2c74886c4504b455f545a57b6e18b7d450295f2a
D-PYTHA <pythamoua@gmail.com>
2025-12-04 11:23:40 +0400

    minor changes

--- app.py
+++ app.py
@@ -238,6 +238,9 @@ def splash():
 @app.route('/home')
 def home():
     return render_template('home.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/admin/passwords')
 def admin_passwords():
